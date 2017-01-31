7aa0c0f83583965448ecd1c68dc084e39f0f1b0d
qi-feng <sely520@gmail.com>
2017-01-31 17:31:03 -0500

    Updated option parser

--- like_3FGL.py
+++ like_3FGL.py
@@ -17,6 +17,11 @@ from UnbinnedAnalysis import *
 from UpperLimits import UpperLimits
 from make3FGLxml import *
 
+from optparse import OptionParser
+
+
+
+
 class analyze3FGL:
     def __init__(self, name='3FGL J2202.7+4217', id_string='BLLac', ra=330.68, dec=42.2778,
                  work_dir='.', d_dir = '.', e_dir = 'events/', emin=100, emax=300000,
@@ -292,9 +297,40 @@ class analyze3FGL:
 
 
 if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-n","--name",dest="name", default='3FGL J2202.7+4217')
+    parser.add_option("-s","--source_name",dest="source_name", default='BLLac')
+    parser.add_option("-r","--ra",dest="ra", default=330.68)
+    parser.add_option("-d","--dec",dest="dec", default=42.2778)
+    parser.add_option("--rad",dest="rad", default=10)
+    parser.add_option("--srcrad",dest="srcrad", default=20)
+    parser.add_option("-l","--list",dest="evlistname", default='@BLLac_events.list')
+    parser.add_option("--scfile",dest="scfilename", default='L1701271500537C62B87191_SC00.list')
+    parser.add_option("-w","--work_dir",dest="work_dir",default='.')
+    parser.add_option("-e","--e_dir",dest="e_dir",default='events/')
+    parser.add_option("--data_dir",dest="d_dir",default='.')
+    parser.add_option("--e_min",dest="e_min",default=100)
+    parser.add_option("--e_max",dest="e_max",default=300000)
+    parser.add_option("--ext_dir",dest="ext_dir",default='/Users/qifeng/Data/Blazars/BLLac/Fermi/Extended_archive_v15/Templates/')
+    #parser.add_option("--do_diffuse",dest="do_diffuse",default=True)
+    parser.add_option("--skip_diffuse", action="store_false", dest="do_diffuse", default=True, help="don't run gtdiff")
+    parser.add_option("-m", "--model",dest="model",default='BLLac_3FGL_modelPL.xml')
+    parser.add_option("--model",dest="modeloutbase",default='BLLac_3FGL_modelPL_out')
+    parser.add_option("-o", "--outfile",dest="fname_DT",default='BLLac_like_results_2016.txt')
+    parser.add_option("-t", "--tag",dest="ev_file_tag",default='10deg2016')
+    parser.add_option("--t_start",dest="t_start",default=495936004.000)
+    parser.add_option("--t_stop",dest="t_stop",default=499392004.000)
+
+    (options, args) = parser.parse_args()
+
     # Defining working directory and source name for analysis
     print('Defining file and source names for LAT analysis.\n')
-    bllac3fgl = analyze3FGL()
+    bllac3fgl = analyze3FGL(name=options.name, id_string=options.source_name, ra=options.ra, dec=options.dec,
+                 work_dir=options.work_dir, d_dir=options.d_dir, e_dir=options.e_dir, emin=options.e_min, emax=options.e_max,
+                 ext_dir=options.ext_dir,
+                 evlistname=options.evlistname, scfilename=options.scfilename,
+                 do_diffuse=options.do_diffuse, ev_file_tag=options.ev_file_tag, fname_DT=options.fname_DT,
+                 model=options.model, modeloutbase=options.modeloutbase, rad=options.rad, srcrad=options.srcrad)
     #t_start = hdulist[1].header['TSTART'] # TSTART time from spacecraft file
     #t_stop = hdulist[1].header['TSTOP'] # TSTOP time from spacecraft file
     #2011-01-01 00:00:00 315532802.000
@@ -302,10 +338,7 @@ if __name__ == '__main__':
     #2013-01-01 00:00:00 378691203.000
     #2014-01-01 00:00:00 410227203.000
     #2015-01-01 00:00:00 441763203.000
-
-    t_start = 495936004.000
     #t_stop = 410227203.000
     #381369603.000 is 2013-02-01
-    t_stop = 499392004.000
 
-    bllac3fgl.run(t_start=t_start, t_stop=t_stop)
+    bllac3fgl.run(t_start=options.t_start, t_stop=options.t_stop)
